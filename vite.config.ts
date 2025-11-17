752b1601592a1a42600c0aaefd87ef3067b303ad
Luka David <ldavi029@uottawa.ca>
2025-11-17 15:58:36 -0500

    Updated package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 export default defineConfig({
   plugins: [react()],
-  resolve: { alias: { '@': path.resolve(__dirname, 'src') } }
-})
+  base: "/LukaDavid04.github.io/",
+  resolve: { alias: { "@": path.resolve(__dirname, "src") } },
+});
